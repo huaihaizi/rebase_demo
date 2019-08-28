67bf56f14ba71c4ea881e597b207da5145437e5f
luxiaoxun <1609236197@qq.com>
2019-08-28 11:11:24 +0800

    master第二次提交

--- app/src/main/java/com/jni/rebasetest/MainActivity.java
+++ app/src/main/java/com/jni/rebasetest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("hhh", "--------------11111-->:");
+        Log.d("hhh", "-----------master---11111-->:");
     }
 }
